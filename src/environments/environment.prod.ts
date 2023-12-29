137cee6eb054e97e45079d33b7c3fa3a809cdabe
Swapnil Opash <sra.opash@gmail.com>
2023-12-29 10:34:07 +0530

    initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  hmr: false,
+  serverUrl: 'https://api.medicaltalk.tube/api/v1/',
+  serverUrl2: 'http://localhost:8080/api/v1/',
+  socketUrl: 'https://api.medicaltalk.tube/',
+  socketUrl2: 'http://localhost:8080/',
+  imgUrl: 'root/storage/uploads',
+
+};
